7c39f4d649743c59f7d446b8c2f215126939f1d3
hanepjiv <hanepjiv@gmail.com>
2016-05-11 14:30:03 +0900

    License

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,13 @@
 /* -*- mode:rust; coding:utf-8-unix; -*- */
 
-// @author hanepjiv <hanepjiv@gmail.com>
-// @since 2016/03/08
-// @date 2016/05/08
-
-// The MIT License (MIT)
-//
-// Copyright (c) <2016> hanepjiv <hanepjiv@gmail.com>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 //! elicit.rs
-//!
+
+//  Copyright 2016 hanepjiv
+//  @author hanepjiv <hanepjiv@gmail.com>
+//  @copyright The MIT License (MIT) / Apache License Version 2.0
+//  @since 2016/03/08
+//  @date 2016/05/11
+
 //! # Examples
 //!
 //! ```
@@ -164,8 +144,7 @@ macro_rules! elicit_define {
                     match self._weak {
                         None            => None,
                         Some(ref x)     => {
-                            Some(Elicit(x.upgrade().
-                                        expect("elicit_from_self")))
+                            Some(Elicit(x.upgrade().expect("elicit_from_self")))
                         }
                     }
                 }
