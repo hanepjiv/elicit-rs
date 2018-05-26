9d4329a0259740b3b32d6a54820b0d47fd0899e8
hanepjiv <hanepjiv@gmail.com>
2018-05-27 07:39:52 +0900

    rustc 1.28.0-nightly (990d8aa74 2018-05-25)

--- src/lib.rs
+++ src/lib.rs
@@ -6,42 +6,43 @@
 //  @author hanepjiv <hanepjiv@gmail.com>
 //  @copyright The MIT License (MIT) / Apache License Version 2.0
 //  @since 2016/03/08
-//  @date 2018/05/13
+//  @date 2018/05/27
 
 // ////////////////////////////////////////////////////////////////////////////
 // attribute  =================================================================
-// 1.27.0-nightly (ff2ac35db 2018-05-12)
-#![deny(
-    absolute_path_starting_with_module, anonymous_parameters,
-    bare_trait_object, missing_copy_implementations,
-    missing_debug_implementations, missing_docs, unnecessary_extern_crate,
-    unused_extern_crates, unused_import_braces, unused_qualifications,
-    unused_results, variant_size_differences, const_err, deprecated,
-    illegal_floating_point_literal_pattern, improper_ctypes,
-    late_bound_lifetime_arguments, non_camel_case_types,
-    non_shorthand_field_patterns, non_snake_case, non_upper_case_globals,
-    no_mangle_generic_items, overflowing_literals, path_statements,
-    patterns_in_fns_without_body, plugin_as_library, private_in_public,
-    private_no_mangle_fns, private_no_mangle_statics,
-    renamed_and_removed_lints, safe_packed_borrows, stable_features,
-    type_alias_bounds, tyvar_behind_raw_pointer, unconditional_recursion,
-    unions_with_drop_fields, unknown_lints, unreachable_code,
-    unreachable_patterns, unstable_name_collision, unused_allocation,
-    unused_assignments, unused_attributes, unused_comparisons,
-    unused_doc_comment, unused_features, unused_imports, unused_macros,
-    unused_must_use, unused_mut, unused_parens, unused_unsafe,
-    unused_variables, while_true, exceeding_bitshifts,
-    incoherent_fundamental_impls, invalid_type_param_default,
-    legacy_constructor_visibility, legacy_directory_ownership, legacy_imports,
-    missing_fragment_specifier, mutable_transmutes, no_mangle_const_items,
-    parenthesized_params_in_types_and_modules, pub_use_of_private_extern_crate,
-    safe_extern_statics, unknown_crate_types
-)]
-#![warn(dead_code, unreachable_pub, unstable_features)]
-#![allow(
-    box_pointers, elided_lifetime_in_path, single_use_lifetime, trivial_casts,
-    trivial_numeric_casts, unsafe_code
-)]
+// rustc 1.28.0-nightly (990d8aa74 2018-05-25)  -------------------------------
+#![deny(absolute_paths_not_starting_with_crate, anonymous_parameters,
+        bare_trait_objects, box_pointers, missing_copy_implementations,
+        missing_debug_implementations, missing_docs, trivial_casts,
+        trivial_numeric_casts, unnecessary_extern_crates, unreachable_pub,
+        unsafe_code, unused_extern_crates, unused_import_braces,
+        unused_labels, unused_lifetimes, unused_qualifications,
+        unused_results, variant_size_differences, dead_code, deprecated,
+        duplicate_associated_type_bindings,
+        illegal_floating_point_literal_pattern, improper_ctypes,
+        late_bound_lifetime_arguments, non_camel_case_types,
+        non_shorthand_field_patterns, non_snake_case, non_upper_case_globals,
+        no_mangle_generic_items, overflowing_literals, path_statements,
+        patterns_in_fns_without_body, plugin_as_library, private_in_public,
+        private_no_mangle_fns, private_no_mangle_statics,
+        safe_packed_borrows, stable_features, trivial_bounds,
+        type_alias_bounds, tyvar_behind_raw_pointer, unconditional_recursion,
+        unions_with_drop_fields, unknown_lints, unreachable_code,
+        unreachable_patterns, unstable_name_collisions, unused_allocation,
+        unused_assignments, unused_attributes, unused_comparisons,
+        unused_doc_comments, unused_features, unused_imports, unused_macros,
+        unused_must_use, unused_mut, unused_parens, unused_unsafe,
+        unused_variables, while_true, const_err, exceeding_bitshifts,
+        incoherent_fundamental_impls, invalid_type_param_default,
+        legacy_constructor_visibility, legacy_directory_ownership,
+        missing_fragment_specifier, mutable_transmutes,
+        no_mangle_const_items, parenthesized_params_in_types_and_modules,
+        pub_use_of_private_extern_crate, safe_extern_statics,
+        unknown_crate_types)]
+#![warn(dead_code, unreachable_pub, renamed_and_removed_lints,
+        unstable_features)]
+#![allow(box_pointers, elided_lifetimes_in_paths, single_use_lifetimes,
+         trivial_casts, trivial_numeric_casts, unsafe_code)]
 // use  =======================================================================
 pub use self::error::Error;
 // mod  =======================================================================
