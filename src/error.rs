9885a723f9729612f44e737dfd387aee177f2e86
hanepjiv <hanepjiv@gmail.com>
2018-06-07 05:18:28 +0900

    Update impl Display for Error

--- src/error.rs
+++ src/error.rs
@@ -6,7 +6,7 @@
 //  @author hanepjiv <hanepjiv@gmail.com>
 //  @copyright The MIT License (MIT) / Apache License Version 2.0
 //  @since 2016/12/31
-//  @date 2018/06/01
+//  @date 2018/06/15
 
 // ////////////////////////////////////////////////////////////////////////////
 // use  =======================================================================
@@ -28,11 +28,7 @@ pub enum Error {
 impl Display for Error {
     // ========================================================================
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        match *self {
-            ref e @ Error::PoisonedRead
-            | ref e @ Error::PoisonedWrite
-            | ref e @ Error::WouldBlock => write!(f, "{:?}", e),
-        }
+        <Self as ::std::fmt::Debug>::fmt(self, f)
     }
 }
 // ============================================================================
