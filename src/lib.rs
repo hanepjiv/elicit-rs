e3137221b66eb693d7809dd54d3abba7bc61729e
hanepjiv <hanepjiv@gmail.com>
2018-06-22 08:25:20 +0900

    Fixes for rustc 1.28.0-nightly (f28c7aef7 2018-06-19)

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,11 @@
 //  @author hanepjiv <hanepjiv@gmail.com>
 //  @copyright The MIT License (MIT) / Apache License Version 2.0
 //  @since 2016/03/08
-//  @date 2018/06/07
+//  @date 2018/06/22
 
 // ////////////////////////////////////////////////////////////////////////////
 // attribute  =================================================================
-// rustc 1.28.0-nightly (4a9c58c6b 2018-06-05)
+// rustc 1.28.0-nightly (f28c7aef7 2018-06-19)
 #![deny(
     absolute_paths_not_starting_with_crate, anonymous_parameters,
     bare_trait_objects, missing_copy_implementations,
@@ -19,23 +19,24 @@
     unused_qualifications, unused_results, variant_size_differences,
     deprecated, duplicate_associated_type_bindings,
     illegal_floating_point_literal_pattern, improper_ctypes,
-    late_bound_lifetime_arguments, non_camel_case_types,
-    non_shorthand_field_patterns, non_snake_case, non_upper_case_globals,
-    no_mangle_generic_items, overflowing_literals, path_statements,
-    patterns_in_fns_without_body, plugin_as_library, private_in_public,
-    private_no_mangle_fns, private_no_mangle_statics, safe_packed_borrows,
-    stable_features, trivial_bounds, type_alias_bounds,
+    intra_doc_link_resolution_failure, late_bound_lifetime_arguments,
+    non_camel_case_types, non_shorthand_field_patterns, non_snake_case,
+    non_upper_case_globals, no_mangle_generic_items, overflowing_literals,
+    path_statements, patterns_in_fns_without_body, plugin_as_library,
+    private_in_public, private_no_mangle_fns, private_no_mangle_statics,
+    safe_packed_borrows, stable_features, trivial_bounds, type_alias_bounds,
     tyvar_behind_raw_pointer, unconditional_recursion, unions_with_drop_fields,
     unknown_lints, unreachable_code, unreachable_patterns,
     unstable_name_collisions, unused_allocation, unused_assignments,
     unused_attributes, unused_comparisons, unused_doc_comments,
     unused_features, unused_imports, unused_macros, unused_must_use,
     unused_mut, unused_parens, unused_unsafe, unused_variables, while_true,
-    const_err, exceeding_bitshifts, incoherent_fundamental_impls,
-    invalid_type_param_default, legacy_constructor_visibility,
-    legacy_directory_ownership, missing_fragment_specifier, mutable_transmutes,
-    no_mangle_const_items, parenthesized_params_in_types_and_modules,
-    pub_use_of_private_extern_crate, safe_extern_statics, unknown_crate_types
+    const_err, duplicate_macro_exports, exceeding_bitshifts,
+    incoherent_fundamental_impls, invalid_type_param_default,
+    legacy_constructor_visibility, legacy_directory_ownership,
+    missing_fragment_specifier, mutable_transmutes, no_mangle_const_items,
+    parenthesized_params_in_types_and_modules, pub_use_of_private_extern_crate,
+    safe_extern_statics, unknown_crate_types
 )]
 #![warn(
     dead_code, renamed_and_removed_lints, unstable_features, unreachable_pub
