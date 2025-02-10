46fd7f9c293cbcf03a5beab900a1cd4c682b6fd6
hanepjiv <hanepjiv@gmail.com>
2025-02-11 01:56:08 +0900

    rustc 1.84.1 (e71f9a9a9 2025-01-27)

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 //  @author hanepjiv <hanepjiv@gmail.com>
 //  @copyright The MIT License (MIT) / Apache License Version 2.0
 //  @since 2016/03/08
-//  @date 2025/01/20
+//  @date 2025/01/21
 
 // ////////////////////////////////////////////////////////////////////////////
 // attribute  =================================================================
